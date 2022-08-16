042d48fe4e34d48a22e428e13543066437986c89
Pavel Morozuk <morozuki@mail.ru>
2022-08-17 00:06:18 +0300

    fixed white line

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -8,7 +8,7 @@ import style from './Skills.module.css';
 const Skills = () => {
   return (
     <div className={style.skillsBlock}>
-      <div className={`${styleContainer} ${style.skillsContainer}`}>
+      <div className={`${styleContainer.container} ${style.skillsContainer}`}>
         <h2 className={style.title}>Skills</h2>
         <div className={style.skills}>
           <Skill />
