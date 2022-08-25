f38b2a55bfd260d5149f9df759c2134bfe9f1c16
Pavel Morozuk <morozuki@mail.ru>
2022-08-25 21:45:50 +0300

    Part 1

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import styleContainer from '../common/styles/Container.module.css';
+
+import style from './Footer.module.css';
+
 const Footer = () => {
   return (
-    <div>
-      <h2>Pavel Moroziuk</h2>
+    <div className={style.blockFooter}>
+      <div className={`${styleContainer.container} ${style.footerContainer}`}>
+        <h2 className={style.title}>Pavel Moroziuk</h2>
+        <div className={style.socialContainer}>
+          <div className={style.social} />
+          <div className={style.social} />
+          <div className={style.social} />
+          <div className={style.social} />
+        </div>
+        <span className={style.rights}>© All rights reserved 2022</span>
+      </div>
     </div>
   );
 };
